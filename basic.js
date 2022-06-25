7e14ad1540aec7ee611e3e58c567ca525de48a9a
MirzaAyon <ayonmirza98@gmail.com>
2022-06-25 10:03:21 +0600

    variable and condition added

---
+++ basic.js
@@ -0,0 +1,24 @@
+// variable
+var deposit = 400;
+// condition
+if (deposit > 500) {
+
+}
+else if (deposit < 200) {
+
+}
+else if (deposit == 500) {
+
+}
+else if (deposit != 500) {
+
+}
+else if (deposit >= 500) {
+
+}
+else if (deposit <= 500) {
+
+}
+else {
+
+}
