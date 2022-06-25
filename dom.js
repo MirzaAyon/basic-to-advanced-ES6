dc8b01397f65ec9ed78590ff338d213f85e1411b
MirzaAyon <ayonmirza98@gmail.com>
2022-06-25 12:33:58 +0600

    new friend added

--- dom.js
+++ dom.js
@@ -19,3 +19,16 @@ function addBackgroundColor() {
         friend.style.backgroundColor = 'lightblue';
     }
 }
+
+
+//add friend
+document.getElementById('add-friend').addEventListener('click', function () {
+    const container = document.getElementById('friend-container');
+    const friendDiv = document.createElement('div');
+    friendDiv.classList.add('friend');
+    friendDiv.innerHTML = `
+            <h3 class="friend-name">New Friend</h3>
+            <p>Quam, sapiente.</p>
+    `;
+    container.appendChild(friendDiv);
+})
