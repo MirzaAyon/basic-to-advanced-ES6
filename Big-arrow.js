d7e28c35aed741a8cf3b01c3ba1016848f2ac987
MirzaAyon <ayonmirza98@gmail.com>
2022-06-25 21:03:35 +0600

    sum and multiply added with function

---
+++ Big-arrow.js
@@ -0,0 +1,12 @@
+//duiti number sum er arrow function
+const add = (num1, num2) => num1 + num2;
+const sum = add(22, 90);
+console.log(sum);
+//multiply
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+const result = multiply(4, 12, 3);
+console.log(result);
+//10 times
+const tenTimes = (num) => num * 10;
+const output = tenTimes(17);
+console.log(output);
