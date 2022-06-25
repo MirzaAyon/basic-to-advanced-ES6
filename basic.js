5a8010a073a628ff794b90d010ca6205909bc217
MirzaAyon <ayonmirza98@gmail.com>
2022-06-25 10:08:35 +0600

    array added

--- basic.js
+++ basic.js
@@ -22,3 +22,12 @@ else if (deposit <= 500) {
 else {
 
 }
+//switch er kaj ta pore derokar hole google theke shikhe nibo
+
+
+// array
+const numbers = [45, 6587, 124, 45, 1, 365];
+const numberCount = numbers.length; //array er modhe koeta upadan ache jante parbo
+numbers.pop(); //last er ta ber kore dewa
+numbers.push(111); //last e ekta add kora
+numbers[2] = 555; //specific position er element change kora jonno
