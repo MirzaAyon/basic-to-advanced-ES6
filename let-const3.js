7910ff91b52c512ee53bfa5e2275de22e4745515
MirzaAyon <ayonmirza98@gmail.com>
2022-06-25 16:01:57 +0600

    let const all concept revised with array,object and loops

---
+++ let-const3.js
@@ -0,0 +1,33 @@
+// const balance = 1200;
+// balance = 200;
+//eta run korte gele error dibe
+
+// let balance = 1200;
+// balance = 20;
+//ekhane r error dibe na
+
+const userName = "janpaki tumi koi";
+const weTogether = 'amr' + userName;
+//eta possible
+// error dibe - identifier userName has already been declared
+
+const numbers = [12, 23, 45, 67, 54];
+// numbers = [456,30,60]; //not allowed
+numbers.push(345); //possible
+numbers[1] = 333; //possible
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    console.log(number);
+    //ekhane problem nai karon age defined korsi but ekhane use korsi
+    sum = sum + number; //sum er maan ta change kortesi
+}
+
+//object
+const student = {
+    roll: 101,
+    name: 'Ayon',
+    job: 'intern'
+}
+student.name = 'Mofazzol'; //possible
+// student = {} //possible na
