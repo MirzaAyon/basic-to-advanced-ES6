adeadd8f1ad944c3994c1a2e0f571da31fbca1b7
MirzaAyon <ayonmirza98@gmail.com>
2022-06-25 20:49:47 +0600

    arrow function added

--- arrow-function2.js
+++ arrow-function2.js
@@ -10,15 +10,23 @@ const sum = add(10, 20);
 console.log(sum);
 
 //func ke variable akareo korte pari
-
+//eta func expression
 const add2 = function add2(num1, num2) {
     return num1 + num2;
 }
 const sum2 = add2(20, 30);
 console.log(sum2);
-
+//etao func expresson but etake anoynimous function bole
 const add3 = function (num1, num2) {
     return num1 + num2;
-} //ei func tae kono naam nai tai eke bole anoynimous functionS
+} //ei func tae kono naam nai tai eke bole anoynimous function
 const sum3 = add3(20, 30);
 console.log(sum3);
+
+//arrow function
+const add4 = (num1, num2) => num1 + num2;
+//eta arrow func ekhane second braket nai, return nai, function keyword nai
+//equal sign and grater than sign
+
+const sum4 = add4(20, 30);
+console.log(sum4);
